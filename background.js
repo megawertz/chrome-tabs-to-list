471c87820e0db36d36dbbf0a67758c9b14a02543
Jason Wertz <jason.wertz@gmail.com>
2016-10-06 20:51:22 -0400

    Finish Options
    * Add more styles
    * Refactor how templates are handled
    * Make sure mime type matches the chosen list language

--- background.js
+++ background.js
@@ -1,13 +1,28 @@
 // TODO: Need a better way to manage values across files
 // Possible values are:
+// 	    list_type_language = [ html | markdown | plain ]
 // 		clipboard_format = [ html | text ]
-// 		list_type = [ ordered | unordered ]
+// 		list_order = [ ordered | unordered ]
 // 		list_item_format = [ title | url | all ]
-
-var _templates = ['<li><a href="{{URL}}"><b>{{TITLE}}</b></a></li>',
-				  '<li><a href="{{URL}}"><b>{{URL}}</b></a></li>',
-				  '<li><a href="{{URL}}"><b>{{TITLE}}</b> [<i>{{URL}}</i>]</a></li>'];
 				  
+var _templates_html = ['<li><a href="{{URL}}"><b>{{TITLE}}</b></a></li>',
+				 	   '<li><a href="{{URL}}"><b>{{URL}}</b></a></li>',
+				       '<li><a href="{{URL}}"><b>{{TITLE}}</b> [<i>{{URL}}</i>]</a></li>'
+				      ];
+
+var _templates_markdown = [ '[__{{TITLE}}__]({{URL}})\n',
+				 			'[__{{URL}}__]({{URL}})\n',
+				 			'[__{{TITLE}}__ {{URL}}]({{URL}})\n'];	
+
+var _templates_plain = ['{{{TITLE}}\n\n',
+				 		'{{URL}}\n\n',
+				 		'{{TITLE}}\n{{URL}}\n\n'];			  
+	  
+// Additional format possibilities
+//
+// dt/dd list (must be both)
+//'<dt><b>{{TITLE}}</b></dt><dd><a href="{{URL}}">[<i>{{URL}}</i>]</a></dd>'
+					  
 var _html = "";
 var _mime = "text/plain";
 
@@ -15,55 +30,81 @@ var _mime = "text/plain";
 // Called on the copy event to put html on the clipboard
 document.addEventListener("copy", function(event) {
 	  event.preventDefault();
-	  console.log(_mime);
 	  event.clipboardData.setData(_mime, _html);
 });
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
 
+	// Easy feedback this action has occured
+	chrome.browserAction.setBadgeText({text: "copy"});
+
 	// Get settings
 	 chrome.storage.sync.get({
-		list_type: 'unordered',
+	 	list_type_language: 'html',
+		list_order: 'unordered',
 		clipboard_format: 'html',
 		list_item_format:'all'
 	  }, function(items) {
 	  
-		var list_type = items.list_type;
-		var list_item_format= items.list_item_format;		
-		
+		var list_order = items.list_order;
+		var list_item_format = items.list_item_format;
+		var list_type_language = items.list_type_language;		
 		_mime = (items.clipboard_format === 'html') ? "text/html" : "text/plain";
+		_html = "";
 
 		chrome.tabs.query( {"currentWindow": true} , function(tabs) {
 
 			// Set type of list
-			_html = (items.list_type === 'ordered') ? "<ol>" : "<ul>";
-
+			var active_templates;
+			if(list_type_language === 'html') {
+				_html = (items.list_order === 'ordered') ? "<ol>" : "<ul>";
+				active_templates = _templates_html;
+			} else if(list_type_language === 'markdown') {
+				active_templates = _templates_markdown;
+			} else {
+				active_templates = _templates_plain;
+			}
+			
 			// Set list item template
 		    var template = "";
 		    if(items.list_item_format === 'title') {
-		    	template = _templates[0];
+		    	template = active_templates[0];
 		    } else if (items.list_item_format === 'url') {
-		    	template = _templates[1];
+		    	template = active_templates[1];
 		    } else {
-		    	template = _templates[2];
+		    	template = active_templates[2];
 		    }
 		    
 		    // Loop through and process template
 			for(var i = 0 ; i < tabs.length ; i++) {
+				
+				if(list_type_language !== 'html' && list_order === 'ordered') {
+					_html +=  (i+1) + ". ";
+			} else if(list_type_language === 'markdown' && list_order === 'unordered') {
+					_html +=  "* ";
+			
+			}
 				var temp = template.replace(/{{URL}}/gi,tabs[i].url);
 				var temp = temp.replace(/{{TITLE}}/gi,tabs[i].title);
 				_html += temp;
 			}		
 			
 			// Set end of list type
-			_html += (items.list_type === 'ordered') ? "</ol>" : "</ul>";;
-		
+			if(list_type_language === 'html') {
+				_html += (items.list_order === 'ordered') ? "</ol>" : "</ul>";;
+			}
+				
 			// Copy it to clipboard
 			document.execCommand('copy');
 		
-			var action_url = "javascript:window.alert('" + _html + "');";
-			chrome.tabs.update(tab.id, {url: action_url});
+			// Remove copy
+			setTimeout(function() {
+		  		chrome.browserAction.setBadgeText({text: ""});
+			}, 600);
+			
+			// var action_url = "javascript:window.alert('" + _html + "');";
+			// chrome.tabs.update(tab.id, {url: action_url});
 	
 		});
 
--- options.js
+++ options.js
@@ -1,15 +1,26 @@
-var list_formats = ["Tab Title",
-					"Url",
-					"Tab Title - <i>[Url]</i>"];
+var list_formats_html = ["<b>Tab Title</b>",
+						 "<b>Url</b>",
+						 "<b>Tab Title</b> <i>[Url]</i>"];
+
+var list_formats_markdown = ["[__Tab Title__](Url)",
+						 	 "[__Url__](Url)",
+						  	 "[__Tab Title__ Url](Url)"];	
+
+var list_formats_plain = ["Tab Title",
+						  "Url",
+						  "Tab Title<br>Url"];							  	 
+
 
 function save_options() {
 
-	var m_list_type = document.getElementById('list_type').value;
+	var m_list_type_language = document.getElementById('list_type_language').value;
+	var m_list_order = document.getElementById('list_order').value;
 	var m_clipboard_format = document.getElementById('clipboard_format').value;
 	var m_list_item_format = document.getElementById('list_item_format').value;
 
 	chrome.storage.sync.set({
-		list_type: m_list_type,
+		list_type_language: m_list_type_language,
+		list_order: m_list_order,
 		clipboard_format: m_clipboard_format,
 		list_item_format:m_list_item_format
 	  }, function() {
@@ -27,19 +38,23 @@ function restore_options() {
 
 	 // Use default value color = 'red' and likesColor = true.
 	  chrome.storage.sync.get({
-		list_type: 'unordered',
+	    list_type_language: 'html',
+		list_order: 'unordered',
 		clipboard_format: 'html',
 		list_item_format:'all'
 	  }, function(items) {
 	  	
 	  	var status = document.getElementById('status');
-		status.textContent = "Current Options: " + items.list_type + 
-							 ", " + items.clipboard_format + 
-							 ", " + items.list_item_format ;
+		status.textContent = "Current Options: " + 
+									items.list_type_language +
+							 ", " + items.list_order + 
+							 ", " + ((items.list_item_format === 'all') ? "title+url" :  items.list_item_format) + 
+							 ", " + items.clipboard_format;
 	  
-		document.getElementById('list_type').value = items.list_type;
+		document.getElementById('list_order').value = items.list_order;
 		document.getElementById('clipboard_format').value = items.clipboard_format;
 		document.getElementById('list_item_format').value = items.list_item_format;
+		document.getElementById('list_type_language').value = items.list_type_language;
 		
 		update_list_format_sampletext()		
 
@@ -49,19 +64,49 @@ function restore_options() {
 
 function update_list_format_sampletext() {
 	// Get currently selected values	
-	var list_type = document.getElementById('list_type').value;
+	var list_order = document.getElementById('list_order').value;
 	var list_item_format_index = document.getElementById('list_item_format').selectedIndex;
+	var list_type_language = document.getElementById('list_type_language').value;
+	
+ 	var clipboard_format_html = document.getElementById('clipboard_format_html');
+ 	var clipboard_format_text = document.getElementById('clipboard_format_text');
+ 	var clipboard_status = document.getElementById('clipboard_feedback');
+ 	
+ 	if(list_type_language === "html") {
+ 		clipboard_status.innerHTML = "";
+ 	
+ 		clipboard_format_html.removeAttribute("hidden");
+ 	} else {	
+		clipboard_status.innerHTML = "* Markdown and Plain Text can only be copied as text";
+	
+		var clipboard_format = document.getElementById('clipboard_format');
+		clipboard_format.value = 'text';
+ 		clipboard_format_html.setAttribute("hidden", true);
+ 	}
+	
 	
 	// Build string
+	// This will get ungainly with lots of formats, need to refactor
 	var content = "";
-	if(list_type === "ordered") {
+	if(list_order === "ordered") {
 		content = "1. ";
 	} else {
-		content = "&bull; "
+		if(list_type_language === "html") {
+			content = "&bull; "
+		} else if(list_type_language === "markdown") {
+			content = "* "
+		}
 	}
 	
-	content += list_formats[list_item_format_index];
-
+	// How to handle multiple formats easily?
+	if(list_type_language === "html") {
+		content += list_formats_html[list_item_format_index];
+	} else if(list_type_language === "markdown") {
+		content += list_formats_markdown[list_item_format_index];
+	} else {
+		content += list_formats_plain[list_item_format_index];
+	}
+	
 	// Set it
 	var status = document.getElementById('sampletext');
 	status.innerHTML = content;
@@ -69,7 +114,8 @@ function update_list_format_sampletext() {
 
 function configItemsOnLoad() {
 	restore_options();
-	document.getElementById('list_type').addEventListener('change', update_list_format_sampletext);
+	document.getElementById('list_order').addEventListener('change', update_list_format_sampletext);
+		document.getElementById('list_type_language').addEventListener('change', update_list_format_sampletext);
 	document.getElementById('list_item_format').addEventListener('change', update_list_format_sampletext);
 	document.getElementById('saveoptions').addEventListener('click', save_options);
 }
